396d5b03a7540eb92702ccbba579a644d9fcd375
Javier Moreno <a01750400@tec.mx>
2022-11-22 19:15:16 -0600

    return only new notifications

--- src/controllers/NotificationController.ts
+++ src/controllers/NotificationController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import AbstractController from "./AbstractController";
 import db from "../models";
+import { Op } from "sequelize";
 
 class NotificationContoller extends AbstractController {
   private static instance: NotificationContoller;
@@ -18,11 +19,31 @@ class NotificationContoller extends AbstractController {
       "/getAllNotifications",
       this.getAllNotifications.bind(this)
     );
+    this.router.get(
+      "/getNewNotifications",
+      this.getNewNotifications.bind(this)
+    );
+
   }
 
   private async getAllNotifications(req: Request, res: Response) {
     try {
       const notifications = await db["Notification"].findAll();
+      res.status(200).send(notifications);
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).send({ message: error.message });
+      } else {
+        res.status(501).send({ message: "External error" });
+      }
+    }
+  }
+
+  private async getNewNotifications(req: Request, res: Response) {
+    try {
+      const notifications = await db["Notification"].findAll(
+        {where: { id_notification: {[Op.gt]: req.query.newest_notification}}}
+      );
       res.status(200).send(notifications);
     } catch (error) {
       if (error instanceof Error) {
